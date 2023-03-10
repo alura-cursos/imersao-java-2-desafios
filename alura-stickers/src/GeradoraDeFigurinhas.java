b7cae1f1a25785663f142340b3083725b73ba7c7
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2023-03-10 18:00:26 -0300

    [aula 2|desafio 4] outline no texto

--- alura-stickers/src/GeradoraDeFigurinhas.java
+++ alura-stickers/src/GeradoraDeFigurinhas.java
@@ -2,10 +2,15 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
+import java.awt.font.TextLayout;
+import java.awt.BasicStroke;
 
 import javax.imageio.ImageIO;
 
@@ -43,7 +48,24 @@ public class GeradoraDeFigurinhas {
         Rectangle2D retangulo = fontMetrics.getStringBounds(texto, graphics);
         int larguraTexto = (int) retangulo.getWidth();
         int posicaoTextoX = (largura - larguraTexto) / 2;
-        graphics.drawString(texto, posicaoTextoX, novaAltura - 100);
+        int posicaoTextoY = novaAltura - 100;
+        graphics.drawString(texto, posicaoTextoX, posicaoTextoY);
+
+
+        FontRenderContext fontRenderContext = graphics.getFontRenderContext();
+        var textLayout = new TextLayout(texto, fonte, fontRenderContext);
+
+        Shape outline = textLayout.getOutline(null);
+        AffineTransform transform = graphics.getTransform();
+        transform.translate(posicaoTextoX, posicaoTextoY);
+        graphics.setTransform(transform);
+
+        var outlineStroke = new BasicStroke(largura * 0.004f);
+        graphics.setStroke(outlineStroke);
+
+        graphics.setColor(Color.BLACK);
+        graphics.draw(outline);
+        graphics.setClip(outline);
 
         // escrever a nova imagem em um arquivo
         ImageIO.write(novaImagem, "png", new File(nomeArquivo));
